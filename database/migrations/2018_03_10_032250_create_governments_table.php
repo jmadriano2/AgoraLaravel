f46d4ae0a1410785b0e16dd60499d4a621c163a8
jmadriano2 <jmadriano2@users.noreply.github.com>
2018-03-10 12:52:00 +0800

    Updated Government Migration

--- database/migrations/2018_03_10_032250_create_governments_table.php
+++ database/migrations/2018_03_10_032250_create_governments_table.php
@@ -14,8 +14,10 @@ class CreateGovernmentsTable extends Migration
     public function up()
     {
         Schema::create('governments', function (Blueprint $table) {
-            $table->increments('id');
+            $table->integer('user_id')->unsigned();
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
